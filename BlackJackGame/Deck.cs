8af7b0b8ab760b7aac32611b1d93fabe51185ec5
Mona Jena <mona.jena@myob.com>
2020-09-03 15:14:42 +1200

    first commit

---
+++ BlackJackGame/Deck.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace BlackJackGame
+{
+    class Deck
+    {
+        
+        //Deck.Generate Deck of Cards -
+        //Deck.Shuffle deck of cards - to randomize
+        //Deck.DrawCard() - return top card from shuffled deck of cards
+        
+        public Deck()
+        {
+            
+        }
+
+        public void GenerateDeck()
+        {
+            
+        }
+        
+    }
+}
---
+++ BlackJackGame/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Black_Jack_Game
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            //Deck.Generate Deck of Cards -
+            //Deck.Shuffle deck of cards - to randomize
+            //Deck.DrawCard() - return top card from shuffled deck of cards
+            //playerHands = DrawCard(newGame) from shuffled deck of cards
+            //dealerHand= draw cards for dealer - get 2 cards from shuffled deck of cards
+            
+            //get players input - hit or stay
+            //if its a hit: draw 1 card and add to playerHands
+                // PrintSum(playerHands)
+            //else: stop
+
+            //dealers turn:
+            //while (sum < 17):
+                    //DrawCard()
+                  //add card to dealerHand
+            //PrintSum(dealerHand) for dealer
+            
+            //Winner() - compare player and dealer sum
+        }
+    }
+}

0d255d78db1ec7586e3a0d571f6e13e215ee0f0b
Mona Jena <mona.jena@myob.com>
2020-10-01 03:33:57 +1300

    fixed console output

--- BlackJackGame/Game.cs
+++ BlackJackGame/Game.cs
@@ -64,29 +64,30 @@ namespace Black_Jack_Game
         public int PlayersTurn(List<Card> playersHand, IDeck deck, int score)
         {
             ifPlayersTurn = true;
-            _newConsole.WriteLine("Player's Turn:");
+            _newConsole.Write("You are currently at " + score + "\nwith the hand ");
             foreach (var i in playersHand)
             {
                 _newConsole.Write(i.ToString() + " ");
             }
-            _newConsole.WriteLine("score: " + score + "\n");
-            
+            _newConsole.WriteLine("");
+
             WinOrLossDuringGame(score, playersHand);
             
             string playersOption = "1";
             while (playersOption == "1" && score < 21)
             {
-                _newConsole.Write("Hit or stay? (Hit = 1, Stay = 0) ");
+                _newConsole.Write("\nHit or stay? (Hit = 1, Stay = 0) ");
                 playersOption = _newConsole.ReadLine();
                 if(playersOption == "1")
                 {
                     playersHand.Add(deck.DrawCard());
+                    score = CalculateScore(playersHand);
+                    _newConsole.Write("\nYou are currently at " + score + "\nwith the hand ");
                     foreach (var i in playersHand)
                     {
                         _newConsole.Write(i.ToString() + " ");
                     }
-                    score = CalculateScore(playersHand);
-                    _newConsole.WriteLine("\nscore: " + score + "\n");
+                    _newConsole.WriteLine("");
                 }
                 else
                 {
@@ -150,24 +151,25 @@ namespace Black_Jack_Game
         public int DealersTurn(List<Card> dealersHand, IDeck deck, int score)
         {
             ifPlayersTurn = false;
-            _newConsole.WriteLine("Dealer's Turn:");
+            _newConsole.Write("\nDealer is at " + score + "\nwith the hand ");
             foreach (var i in dealersHand)
             {
                 _newConsole.Write(i.ToString() + " ");
             }
-            _newConsole.WriteLine("score: " + score + "\n");
-            
+            _newConsole.WriteLine("");
+
             WinOrLossDuringGame(score, dealersHand);
             
             while (score < 17 && (ifGameOver == false))
             {
                 dealersHand.Add(deck.DrawCard());
+                score = CalculateScore(dealersHand);
+                _newConsole.Write("\nDealer is at " + score + "\nwith the hand ");
                 foreach (var i in dealersHand)
                 {
                     _newConsole.Write(i.ToString() + " ");
                 }
-                score = CalculateScore(dealersHand);
-                _newConsole.WriteLine("score: " + score + "\n");
+                _newConsole.WriteLine("");
             }
 
             WinOrLossDuringGame(score, dealersHand);
@@ -180,21 +182,24 @@ namespace Black_Jack_Game
         {
             if (score == 21)
             {
-                var result = ifPlayersTurn ? "Black Jack! You beat the dealer!" : "Black Jack! Dealer wins!";
+                var result = ifPlayersTurn ? "\nBlack Jack! You beat the dealer!" : "Black Jack! Dealer wins!";
                 ifGameOver = true;
                 _newConsole.WriteLine(result);
             }
             else if (score > 21)
             {
-                _newConsole.WriteLine("You are at currently at Bust with the hand:");
+                var result = ifPlayersTurn ? "\nYou are at currently at Bust with the hand " : "\nDealer is currently at Bust with the hand ";
+                ifGameOver = true;
+                _newConsole.WriteLine(result);
                 foreach (var i in gamersHand)
                 {
                     _newConsole.Write(i.ToString() + " ");
                 }
+                _newConsole.WriteLine("");
 
-                var result = ifPlayersTurn ? "\nDealer Wins!" : "\nPlayer Wins!";
+                var result2 = ifPlayersTurn ? "\nDealer Wins!" : "\nPlayer Wins!";
                 ifGameOver = true;
-                _newConsole.WriteLine(result);
+                _newConsole.WriteLine(result2);
             }
         }
 
